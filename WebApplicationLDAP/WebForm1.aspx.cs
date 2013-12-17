36f3d31d4d769ca345ffd8d7a6378005fb01b633
msa27 <msa.27@hotmail.com>
2013-12-17 22:25:06 +0100

    t

--- WebApplicationLDAP/WebForm1.aspx.cs
+++ WebApplicationLDAP/WebForm1.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace WebApplicationLDAP
 {
@@ -12,22 +13,59 @@ namespace WebApplicationLDAP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string ldap = "LDAP://172.19.127.19/ou=people,dc=ic-itcr,dc=ac,dc=cr";
+
+            //string ldap = "LDAP://192.168.1.250/ou=people,dc=ic-itcr,dc=ac,dc=cr";
+            //string pwd = "123456";          
+            //string ldap = "LDAP://172.19.127.19/ou=people,dc=ic-itcr,dc=ac,dc=cr";
+            //string pwd = "Solaris2013";
+            string ldap = "LDAP://192.168.0.112/dc=ic-itcr,dc=ac,dc=cr";
+            string pwd = "123456";
             string usr = "cn=admin,dc=ic-itcr,dc=ac,dc=cr";
-            string pwd = "Solaris2013";
             IsAuthenticated(ldap, usr, pwd);
         }
-       
- 
+
+
         public static bool IsAuthenticated(string ldap, string usr, string pwd)
         {
             bool authenticated = false;
- 
+
             try
             {
                 DirectoryEntry entry = new DirectoryEntry(ldap, usr, pwd, AuthenticationTypes.FastBind);
                 object nativeObject = entry.NativeObject;
                 authenticated = true;
+
+                DirectorySearcher search = new DirectorySearcher(entry);
+
+                search.PropertiesToLoad.Add("userPassword");
+                search.PropertiesToLoad.Add("uid");
+                //search.PropertiesToLoad.Add("memberOf");                
+                //search.Filter = ("(|(objectClass=inetOrgPerson)(objectCategory=user))");
+                //search.Filter = "(&(objectCategory=user)(memberOf=cn=ests,ou=people,dc=ic-itcr,dc=ac,dc=cr))";
+                //search.SizeLimit = 1000; 
+                //search.Filter = "(memberOf=cn=ests,ou=group,dc=ic-itcr,dc=ac,dc=cr)";
+                search.Filter = "(&(memberUid=misalazar2)(cn=users))";
+                foreach (SearchResult result in search.FindAll())
+                {
+                    if (result != null)
+                    {
+                        DirectoryEntry de = result.GetDirectoryEntry();
+                        object x = de.Properties;
+                        string uid = de.Properties["userPassword"].Value.ToString();
+                        uid = uid.Trim();
+                        uid = uid.ToLower();
+                    }
+                }
+
+                DirectorySearcher searcher = new DirectorySearcher(entry);
+                searcher.Filter = "(uid=misalazar)";
+                searcher.PropertiesToLoad.Add("uid");
+
+                SearchResult result2 = searcher.FindOne();
+                if (result2 != null)
+                    Console.WriteLine("Found");
+                else
+                    Console.WriteLine("Not found");
             }
             catch (DirectoryServicesCOMException cex)
             {
